1eb9f9ce16929fed715306839ebde3cc29b803d9
nkellerdesign <nkellerdesign@gmail.com>
2015-05-18 22:23:10 -0400

    Got the file structure of the new rugby site. It includes the bourbon, neat and bitters framework. It also has the php include files and the start of the header and the footer. I don't know if I actually want this to be the way the index.php to have a header but if I'm going to repeat the header and footer then it makes the most sense in the DRY programming.

---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>FWRFC</title>
+  <meta name="description" content="Frederick Women's Rugby Football Club">
+  <meta name="author" content="Nicole Keller">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" type="text/css" href="css/reset.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+ 
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    include("inc/header.php");
+?>
+
+<div class=""
+
+
+
+
+<?php
+    include("inc/footer.php");
+?>
