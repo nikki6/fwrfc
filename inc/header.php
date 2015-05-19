983af2fb78fed556da04329696e69c36e749e686
Nikki <dkn5678@gmail.com>
2015-05-19 15:58:43 -0400

    Started working on some new ideas.

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
-  <link rel="stylesheet" type="text/css" href="css/reset.css">
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/main.css">
 
 
--- index.php
+++ index.php
@@ -1,12 +1,56 @@
-<?php 
-    include("inc/header.php");
-?>
+<!doctype html>
+<html lang="">
 
-<div class=""
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Frederick Women's Rugby Football Club</title>
+    <link rel="shortcut icon" href="">
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script type="text/javascript" src="js/mainJS.js"></script>
+
+    <!--[if IE]>
+        <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div id="js-parallax-window" class="parallax-window">
+        <div class="parallax-static-content">
+            <h1>Frederick Women's Rugby Football Club</h1>
+            <p>since 2004</p>
+        </div>
+        <div id="js-parallax-background" class="parallax-background"></div>
+    </div>
+   
+<!--    Second Section  -->
+    <div class="grid-items">
+    
+    <a href="javascript:void(0)" class="grid-item">
+        <span class="oi" data-glyph="calendar" title="calendar"></span>
+        <h1>Match Schedule</h1>
+        <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
+    </a>
+    <a href="javascript:void(0)" class="grid-item">
+        <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_3.png" alt="">
+        <h1>The Team</h1>
+        <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
+    </a>
+    <a href="javascript:void(0)" class="grid-item">
+        <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="">
+        <h1>Media</h1>
+        <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
+    </a>
+</div>
 
 
+        
+</body>
 
-
-<?php
-    include("inc/footer.php");
-?>
+</html>
