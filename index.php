fef4fe783397e10c0fe6b3e1d6ee2973c8ef07b5
Nikki <dkn5678@gmail.com>
2015-06-01 15:56:29 -0400

    Built a good portion of the new layout...However I'm not sold on the final product yet...

--- index.php
+++ index.php
@@ -22,36 +22,94 @@
 </head>
 
 <body>
-    <div id="js-parallax-window" class="parallax-window">
-        <div class="parallax-static-content">
-            <h1>Frederick Women's Rugby Football Club</h1>
-            <p>since 2004</p>
+
+<!--    This is the Body   -->
+    <div class="container-fluid">
+        <div class="row">
+            <div id="js-parallax-window" class="parallax-window col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="parallax-static-content">
+                    <h1>Frederick Women's Rugby Football Club</h1>
+                    <p>since 2004</p>
+                </div>
+                <div id="js-parallax-background" class="parallax-background"></div>
+            </div>
         </div>
-        <div id="js-parallax-background" class="parallax-background"></div>
-    </div>
-   
-<!--    Second Section  -->
-    <div class="grid-items">
-    
-    <a href="javascript:void(0)" class="grid-item">
-        <span class="icono-calendar" title="calendar"></span>
-        <h1>Match Schedule</h1>
-        <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
-    </a>
-    <a href="javascript:void(0)" class="grid-item">
-        <span class="icono-user" title="team"></span>        
-        <h1>The Team</h1>
-        <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
-    </a>
-    <a href="javascript:void(0)" class="grid-item">
-        <span class="icono-image" title="images"></span><span class="icono-video" title="images"></span>
-        <h1>Media</h1>
-        <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
-    </a>
-</div>
-
-
+        <!--    Second Section  -->
+        <div class="row">
+            <div class="col-xs-12 col-sm-10 col-md-6 col-lg-6 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
+                <h2>About Us</h2>
+                <p>Thanks for checking out the Frederick Women's Rugby webpage. The FWRFC was formed in Fall 2004 by a team of fun, enthusiastic women. We play almost year–round, with a competitive 15's season in the spring and fall, a competitive summer 7's season and some fitness training during the winter months. Plus we have social events, retreats and community projects throughout the year. It's always a great time for new players to join us! All experience levels are welcome and no previous knowledge of the sport is necessary! Check out the About Rugby section for a brief introduction to the sport.</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-md-offset-1 col-lg-offset-1">
+                <img alt="The Team" src="img/smallrugbypitch.png" class="img-responsive">
+            </div>
+        </div>
+        <!--    Third Section   -->
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-md-offset-1 col-lg-offset-1">
+                <h1>Practice Information</h1>
+                <p>Currently we are in our Summer 7s season so we are practicing every Tuesday and Thursday at our home pitch.</p>
+                <address>
+                    <strong>Walnut Ridge Park</strong><br>
+                        1166 Rocky Springs Rd<br>
+                        Frederick, MD 21702<br>
+                </address>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                <h1>Upcoming Events</h1>
+                 <ul>
+                     <li>June 13 - Pax River 10s Game</li>
+                     <li>June - 7s Match</li>
+                     <li>July 25 - Pig Roast</li>
+                     <li>July - 7s Match</li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                <h1>Current Schedule</h1>
+                <div class="table-responsive">
+                    <table class="table table-condens table-striped table-hover">
+                        <thead>
+                            <th>Date</th>
+                            <th>Opponent</th>
+                            <th>Home or Away</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>June 13</td>
+                                <td>Pax River</td>
+                                <td>Away - Pax River</td>
+                            </tr>
+                            <tr>
+                                <td>June </td>
+                                <td>Steel City 7s</td>
+                                <td>Away - Pittsburg</td>
+                            </tr>
+                            <tr>
+                                <td>July</td>
+                                <td>Harrisburg 7s</td>
+                                <td>Away - Harrisburg</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
+        </div>
+        <!--   Fourth Section-->
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3">
+                <h2>Contact Us</h2>
+                <address>
+                    <strong>Vice President</strong><br>
+                    <a href="mailto:#">first.last@example.com</a>
+                </address>
+            </div>
+        </div>
         
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
